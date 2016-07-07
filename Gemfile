02353f10205a81940b1112511f9f7a2d3cd9a4ca
Colin Shea <colin@evaryont.me>
2016-07-06 17:39:57 -0700

    import zonefile.rb into the repository

--- Gemfile
+++
@@ -1,3 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'zonefile', '~> 1.5', github: 'boesemar/zonefile'
--- lib/environment.rb
+++ lib/environment.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
-require 'zonefile'
 require 'yaml'
 
 require_relative "hash_ext"
 require_relative "zone"
 require_relative "zone_generator"
+require_relative 'zonefile'
