241d01d52408ff09cb8712fc6940f7933b2cc0d1
Colin Shea <colin@evaryont.me>
2016-07-06 19:39:10 -0700

    don't need this mkdir call anymore

--- bin/run.rb
+++ bin/run.rb
@@ -2,11 +2,6 @@
 
 require_relative '../lib/environment'
 
-# Create a temp directory for the workspace if it doesn't exist already
-if !File.directory? File.join(Dir.pwd, 'tmp')
-  Dir.mkdir('tmp')
-end
-
 generator = ZoneGenerator.new Dir.pwd
 generator.generate
 generator.deploy
